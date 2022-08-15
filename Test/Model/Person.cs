5a3506d16c9bc328f25f68fc9fbf7d07b0ba01c0
Luan Ducate <luan.ducate@levva.io>
2022-08-15 10:39:21 -0300

    Initial Commit

--- Test/Model/Person.cs
+++ Test/Model/Person.cs
@@ -1,9 +1,13 @@
-﻿namespace Test.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Model
 {
     public class Person
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
     }
 }
